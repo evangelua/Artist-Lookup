99538b258f7ea433a0b52507c7d71d7007b27827
Alex Evangelou <aevangelou10@gmail.com>
2018-01-05 05:09:02 +0000

    initial commit

---
+++ src/Main.py
@@ -0,0 +1,14 @@
+import requests # learn more: https://python.org/pypi/requests
+from bs4 import BeautifulSoup # learn more: https://python.org/pypi/bs4
+import re
+
+base_url = 'https://genius.com/Eminem-believe-lyrics'
+r = requests.get(base_url)
+
+soup = BeautifulSoup(r.text, 'html.parser')
+
+lyrics = str(soup.find(class_="lyrics").text)
+
+n_lyrics = set(re.split('\[.*\]|\n| |, ', lyrics.lower()))
+n_lyrics.pop()
+print (len(n_lyrics))
