d1c870f8c872c57e08f643a019c3b3f884b0f27a
Alex Evangelou <aevangelou10@gmail.com>
2018-01-07 01:08:16 -0500

    test

--- src/Main.py
+++ src/Main.py
@@ -2,13 +2,33 @@ import requests # learn more: https://python.org/pypi/requests
 from bs4 import BeautifulSoup # learn more: https://python.org/pypi/bs4
 import re
 
-base_url = 'https://genius.com/Eminem-believe-lyrics'
-r = requests.get(base_url)
+name = input("Enter name of Artist: ")
+base_url = 'https://www.lyricsondemand.com/' + name[0] + '/' + name + 'lyrics'
+total_set = {}
 
-soup = BeautifulSoup(r.text, 'html.parser')
-lyrics = str(soup.find(class_="lyrics").text)
+try:
+    song_request = requests.get(base_url)
+except:
+    print('Something when wrong in finding songs belonging to the artist')
 
-n_lyrics = set(re.split('\[.*\]|\n| |, ', lyrics.lower()))
-n_lyrics.pop()
+all_soup = BeautifulSoup(song_request.text, 'html.parser')
+all_lyrics = all_soup.find_all(class_="Highlight")
 
-print (len(n_lyrics))
+for i in all_lyrics:
+    if not re.search(r'\.\.\/\.\.|#|album', str(i)):
+        song_url = 'https://www.lyricsondemand.com/' + name[0] + '/' + name + 'lyrics/' + i.a['href']
+
+        try:
+            song_request = requests.get(song_url)
+        except:
+            print('Something when wrong in finding songs belonging to the artist')
+
+        song_soup = BeautifulSoup(song_request.text, 'html.parser')
+        lyrics = song_soup.find(class_="lcontent").text
+
+        n_lyrics = set(re.split('\[.*\]|\n| |, |\(.*\)', lyrics.lower()))
+        n_lyrics.pop()
+
+        total_set | n_lyrics
+
+print (len(total_set))
